152ce61a57b5531c9e9fd18554cbbec5a830a323
Justin Wolfe <justin.wolfe@gmail.com>
2013-11-16 10:59:33 -0500

    menu switch works for settings, just have to copy it for other event handlers and test

--- writersblocks.js
+++ writersblocks.js
@@ -2,6 +2,7 @@
 var spacesTyped = 0;
 var textDisplayed = false;
 var menuDisplayed = false;
+var currentMenu = "";
 var obliqueString = "Abandon normal instruments,Accept advice,Accretion,A line has two sides,Allow an easement (an easement is the abandonment of a stricture),Always first steps,Are there sections?  Consider transitions,Ask people to work against their better judgement,Ask your body,Assemble some of the elements in a group and treat the group,Balance the consistency principle with the inconsistency principle,Be dirty,Be extravagant,Be less critical more often,Breathe more deeply,Bridges -build -burn,Cascades,Change instrument roles,Change nothing and continue with immaculate consistency,Children -speaking -singing,Cluster analysis,Consider different fading systems,Consult other sources -promising -unpromising,Courage!,Cut a vital connection,Decorate,Define an area as 'safe' and use it as an anchor,Destroy -nothing -the most important thing,Discard an axiom,Disciplined self-indulgence,Disconnect from desire,Discover the recipes you are using and abandon them,Distorting time,Do nothing for as long as possible,Don't be afraid of things because they're easy to do,Don't be frightened of cliches,Don't be frightened to display your talents,Don't break the silence,Don't stress one thing more than another,Do something boring,Do the words need changing?,Do we need holes?,Emphasise differences,Emphasise repetitions,Emphasise the flaws,Fill every beat with something,From nothing to more than nothing,Ghost echoes,Give the game away,Give way to your worst impulse,Go outside. Shut the door.,Go slowly all the way round the outside,Go to an extreme,Move back to a more comfortable place,Honor thy error as a hidden intention,How would you have done it?,Humanise something free of error,Idiot glee (?),Imagine the piece as a set of disconnected events,Infinitesimal gradations,Intentions -nobility of -humility of -credibility of,In total darkness,In a very large room,Very quietly,Into the impossible,Is it finished?,Is the intonation correct?,Is there something missing?,It is quite possible (after all),Just carry on,Listen to the quiet voice,Look at the order in which you do things,Look closely at the most embarrassing details and amplify them,Lost in useless territory,Lowest common denominator,Make a blank valuable by putting it in an exquisite frame,Make an exhaustive list of everything you might do and do the last thing on the list,Make a sudden destructive unpredictable action; incorporate,Mechanicalise something idiosyncratic,Mute and continue,Not building a wall but making a brick,Once the search is in progress something will be found,Only a part not the whole,Only one element of each kind,(Organic) machinery,Overtly resist change,Question the heroic approach,Remember those quiet evenings,Remove ambiguities and convert to specifics,Remove specifics and convert to ambiguities,Repetition is a form of change,Retrace your steps,Revaluation (a warm feeling),Reverse,Short circuit (example; a man eating peas with the idea that they will improve his virility shovels them straight into his lap),Simple subtraction,Simply a matter of work,State the problem in words as clearly as possible,Take a break,Take away the elements in order of apparent non-importance,The inconsistency principle,The most important thing is the thing most easily forgotten,The tape is now the music,Think of the radio,Tidy up,Towards the insignificant,Trust in the you of now,Turn it upside down,Use an old idea,Use an unacceptable colour,Use fewer notes,Use filters,Use 'unqualified' people,Water,What are the sections sections of? Imagine a caterpillar moving,What are you really thinking about just now?,What is the reality of the situation?,What mistakes did you make last time?,What wouldn't you do?,What would your closest friend do?,Work at a different speed,Would anybody want it?,You are an engineer,You can only make one dot at a time,You don't have to be ashamed of using your own ideas"
 var obliqueArray = new Array();
 var randomWords = new Array();
@@ -93,9 +94,11 @@ var WBsettings = {
 			if (keyPressed == 49){
 				event.preventDefault();
 				if (menuDisplayed == false){
-					appendMenu("main_menu");
-				} else {
+					appendMenu("settings");
+				} else if (menuDisplayed == true && currentMenu == "settings"){
 					clearMenu();		
+				} else if (menuDisplayed == true && currentMenu != "settings"){
+					appendMenu("settings");
 				};
 			} else if (keyPressed == 50 || keyPressed == 51){
 				event.preventDefault();
@@ -227,7 +230,7 @@ function addMenuListeners(){
 function updateMenu(type){
 	switch(type)
 	{
-		case "main_menu":
+		case "settings":
 			$("#visibleTextSelect").val(WBsettings.visibleText);
 			$("#typewriterSelect").val(WBsettings.typewriterMode);
 			$("#wordTargetInput").val(wordTarget);
@@ -245,9 +248,11 @@ function updateMenu(type){
 };
 
 function appendMenu(type){
+	$("#menuDiv").html("");
 	switch(type)
 	{
-		case "main_menu":
+		case "settings":
+			currentMenu = "settings";
 			$("#menuDiv").append("\
 			<div id='menuDisplay'>\
 				<div id='titleContainer'><b>writer's blocks</b>: a drafting tool</div>\
@@ -272,12 +277,13 @@ function appendMenu(type){
 					<div id='aboutText' class='hotKey'>made by <a href='mailto:justin.wolfe@gmail.com'>justin wolfe</a> using <a href='http://www.jquery.com/'>jQuery</a>, <a href='http://www.steamdev.com/zclip/'>zClip</a>, <a href='http://www.wordnik.com'>Wordnik</a>, <a href='https://samdutton.wordpress.com/2011/03/09/shakespeares-sonnets-in-json-format/'>JSON Sonnets</a>, and <a href='http://somedrafts.com/yournextsentence/'>your next sentence</a></div>\
 				</div>\
 			</div>");
-			updateMenu("main_menu");
+			updateMenu("settings");
 			addMenuListeners();
 			$("#menuDiv").fadeIn(250, function(){});
 			menuDisplayed = true;	
 		break;
 		case "wordnik":
+			currentMenu = "wordnik";
 			$("#menuDiv").append("\
 			<div id='dictionaryDisplay'>\
 				<div id='wordDisplay'></div>\
@@ -333,6 +339,7 @@ function appendMenu(type){
 		break;	
 		case "email":
 			parseHTMLtoString();
+			currentMenu = "email";
 			$("#menuDiv").append("\
 			<div id='emailDisplay'>\
 				<div id='titleDisplay'><div id='innerTitle'>title: <input type='text' id='processEmailTitle'></input></div></div>\
@@ -426,6 +433,7 @@ function appendMenu(type){
 			});
 		break;	
 		case "sonnet":
+		currentMenu = "sonnet";
 			$.getJSON('sonnets.json', function(data) {
 				console.log(data);
 				var randomSonnet = Math.floor((Math.random()*data.length)+0);
@@ -438,6 +446,7 @@ function appendMenu(type){
 			menuDisplayed = true;
 		break;
 		case "oblique":
+		currentMenu = "oblique";
 			var randomOblique = Math.floor((Math.random()*obliqueArray.length)+0);
 			$("#menuDiv").append("<div id='obliqueStrategyDisplay'></div>");
 			$("#obliqueStrategyDisplay").text(obliqueArray[randomOblique]); 				
@@ -445,6 +454,7 @@ function appendMenu(type){
 			menuDisplayed = true;
 		break;
 		case "timer":
+		currentMenu = "timer";
 			if (timerStarted == false){
 				$("#menuDiv").append("\
 				<div id='timerDisplay'>\
@@ -473,18 +483,16 @@ function appendMenu(type){
 	};
 };
 
-function main_menu(){
-};
-
 function clearMenu(){
 	$("#menuDiv").html("");
 	$("#menuDiv").css('display', 'none');
 	menuDisplayed = false;
+	currentMenu = "";
 	$("#processSpace").focus();
 };
 
 function switchMenu(){
-	$("#menuDiv").html("");
+
 };
 
 function check_email(email){  
