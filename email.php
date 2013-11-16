9b1c7410bf6618171293c9c6dcf1bf4a677a95af
Justin Wolfe <justin.wolfe@gmail.com>
2013-11-16 07:16:32 -0500

    added input filtering

--- email.php
+++ email.php
@@ -2,9 +2,12 @@
 $blockText = $_POST['blockText'];
 $blockEmail = $_POST['blockEmail'];
 $blockTitle = $_POST['blockTitle'];
-$to = $blockEmail;
-$subject = $blockTitle;
-$message = $blockText;
+$blockTextFiltered = filter_var($blockText, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+$blockEmailFiltered = filter_var($blockText, FILTER_SANITIZE_EMAIL);
+$blockTitleFiltered = filter_var($blockText, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+$to = $blockEmailFiltered;
+$subject = $blockTitleFiltered;
+$message = $blockTextFiltered;
 $from = "writers_blocks@botetourt.dreamhost.com";
 $headers = "From: Justin Wolfe" . $from;
 mail($to,$subject,$message,$headers);
--- storage/save2.php
+++ storage/save2.php
@@ -1,8 +1,9 @@
 <?php
 $blockText = $_POST['blockText'];
+$blockTextFiltered = filter_var($blockText, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 $blockName = $_POST['blockName'];
 $fileHandler = fopen($blockName, 'w');
-fwrite($fileHandler, $blockText);
+fwrite($fileHandler, $blockTextFiltered);
 fclose($fileHandler);
 echo "written";
 ?>
